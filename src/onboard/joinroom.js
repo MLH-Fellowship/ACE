262a2e8445560fd36008f5cf58fd9be2d213907b
Suhani Chawla <suhanichawla2000@gmail.com>
2021-03-16 22:13:16 +0530

    ui of landing screen

--- src/onboard/joinroom.js
+++ src/onboard/joinroom.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import JoinGame from './joingame'
 import ChessGame from '../chess/UI/chessgame'
-import ACE from '../chess/assets/ACE.png'
 
 /**
  * Onboard is where we create the game room.
@@ -16,16 +15,6 @@ class JoinRoom extends React.Component {
     constructor(props) {
         super(props);
         this.textArea = React.createRef();
-    }
-
-    typingUserName = () => {
-        // grab the input text from the field from the DOM 
-        const typedText = this.textArea.current.value
-        
-        // set the state with that text
-        this.setState({
-            inputText: typedText
-        })
     }
 
     render() {
--- src/onboard/onboard.js
+++ src/onboard/onboard.js
@@ -12,7 +12,7 @@ const socket  = require('../services/socket').socket
 
 class CreateNewGame extends React.Component {
     state = {
-        didGetUserName: false,
+        didGameStart: false,
         inputText: "",
         gameId: ""
     }
@@ -49,22 +49,41 @@ class CreateNewGame extends React.Component {
 
         return (<React.Fragment>
             {
-                this.state.didGetUserName ? 
+                this.state.didGameStart ? 
                 <Redirect to = {"/game/" + this.state.gameId}><button className="btn btn-success" style = {{marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px"}}>Start Game</button></Redirect>
 
             :
-               <div>
+               <div style={{height:'100vh',display:'flex',flexDirection:'column',justifyContent:'center'}}>
+                   <div style={{textAlign:'center'}}>
                     <img src={ACE} height="200px" width="200px"></img>
-                    <button className="btn btn-primary" 
-                        style = {{marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px"}} 
-                        onClick = {() => {
-                            this.props.didRedirect() 
-                            // this.props.setUserName(this.state.inputText) 
-                            this.setState({
-                                didGetUserName: true
-                            })
-                            this.send()
-                        }}>Submit</button>
+                    </div>
+                    <div style={{textAlign:'center'}}>
+                        <h3>Instructions</h3>
+                        <p style={{marginLeft:'30%',textAlign:'left'}}>
+                            1. Non est ad consectetur ut ea commodo id nostrud.
+                            <br/>
+                            2. Aliqua sint do duis amet ut officia.
+                            <br/>
+                            3. Aute magna non sint aute do.
+                            <br/>
+                            4. Duis irure ipsum aute non quis dolor.
+                            <br/>
+                            5. Voluptate tempor amet dolor pariatur sint dolor exercitation nisi aliqua ex est ut eiusmod.
+                            <br/>
+                        </p>
+                    </div>
+                    <div style={{textAlign:'center'}}>
+                        <button className="btn btn-primary" 
+                            style = {{ width: "120px"}} 
+                            onClick = {() => {
+                                this.props.didRedirect() 
+                                this.setState({
+                                    didGetUserName: true
+                                })
+                                this.send()
+                            }}>Start Game
+                        </button>
+                    </div>
                 </div>
             }
             </React.Fragment>)
